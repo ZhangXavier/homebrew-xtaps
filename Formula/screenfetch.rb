8eb479a592d802977572afa4af743fd54793e9bf
zhangxavier <me@zxavier.com>
2022-03-15 17:14:50 +0800

    delete bottle :unneeded

--- Formula/screenfetch.rb
+++ Formula/screenfetch.rb
@@ -1,12 +1,14 @@
 class Screenfetch < Formula
     desc "Generate ASCII art with terminal, shell, and OS info"
     homepage "https://github.com/KittyKatt/screenFetch"
-    url "https://file.zxavier.com/screenFetch-3.9.1-21.03.23.tar.gz"
-    sha256 "10b40c733aa539f726bfd0905067a20572dd58ffa0246b8ebf511d7f5160c16a"
+    url "https://file.zxavier.com/screenFetch/screenFetch-3.9.1-21.05.24.tar.gz"
+    sha256 "fc264300790e82344c24b73bb8acc51b16ab9c8e4bf6f815d3c023c3f7ed93bf"
     license "GPL-3.0"
     head "https://github.com/KittyKatt/screenFetch.git", shallow: false
   
-    bottle :unneeded
+    # `screenfetch` contains references to `/usr/local` that
+    # are erronously relocated in non-default prefixes.
+    # pour_bottle? only_if: :default_prefix
   
     def install
       bin.install "screenfetch-dev" => "screenfetch"
@@ -17,4 +19,3 @@ class Screenfetch < Formula
       system "#{bin}/screenfetch"
     end
   end
-  
